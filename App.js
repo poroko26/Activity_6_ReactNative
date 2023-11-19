94f36b1974deebfc9944d8cd96a837ee44f0fdd9
poroko26 <arnoldjamesfelis@gmail.com>
2023-11-19 10:06:45 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import data from './src/data';
 
 export default function App() {
+
+  const accountTable = () => (
+    <View style={styles.tableContainer}>
+      <Text style={styles.tableLabel}>Account Table</Text>
+      <View style={styles.tableHeader}>
+        <Text style={{width: '15%'}}>ID</Text>
+        <Text style={{width: '30%'}}>Username</Text>
+        <Text style={{width: '30%'}}>Password</Text>
+        <Text style={{width: '25%'}}>UserType</Text>
+      </View>
+      {data.map((datas, index) => (
+        <View key={datas.id} style={index % 2 === 0 ? styles.tableDataEven : styles.tableData}>
+          <Text style={{width: '15%'}}>{datas.id}</Text>
+          <Text style={{width: '30%'}}>{datas.username}</Text>
+          <Text style={{width: '30%'}}>{datas.password}</Text>
+          <Text style={{width: '25%'}}>{datas.usertype}</Text>
+        </View>
+      ))}
+    </View>
+  );
+
+  const userTable = () => (
+      <View style={styles.userContainer}>
+        <Text style={styles.tableLabel}>User Table</Text>
+        <ScrollView horizontal={true}>
+          <View>
+            <View style={styles.tableHeader}>
+              <Text style={{ flex: 1, minWidth: 100 }}>ID</Text>
+              <Text style={{ flex: 2, minWidth: 200 }}>Firstname</Text>
+              <Text style={{ flex: 2, minWidth: 200 }}>Lastname</Text>
+              <Text style={{ flex: 1, minWidth: 100 }}>Course</Text>
+              <Text style={{ flex: 2, minWidth: 200 }}>YearAndSection</Text>
+            </View>
+            <ScrollView>
+              {data.map((datas, index) => (
+                <View key={datas.id} style={index % 2 === 0 ? styles.tableDataEven : styles.tableData}>
+                  <Text style={{ flex: 1, minWidth: 100 }}>{datas.id}</Text>
+                  <Text style={{ flex: 2, minWidth: 200 }}>{datas.name.firstname}</Text>
+                  <Text style={{ flex: 2, minWidth: 200 }}>{datas.name.lastname}</Text>
+                  <Text style={{ flex: 1, minWidth: 100 }}>{datas.course}</Text>
+                  <Text style={{ flex: 2, minWidth: 200 }}>{datas.yearAndSection}</Text>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+  );
+
+  const studentTable = () => (
+    <View style={styles.studentContainer}>
+      <Text style={styles.tableLabel}>Students Table</Text>
+      <View style={styles.tableHeader}>
+              <Text style={{ width: '15%' }}>ID</Text>
+              <Text style={{ width: '50%' }}>Name</Text>
+              <Text style={{ width: '30%' }}>Course</Text>
+            </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => (
+          <View style={index % 2 === 0 ? styles.tableDataEven : styles.tableData}>
+            <Text style={{ width: '15%' }}>{item.id}</Text>
+            <Text style={{ width: '50%' }}>{`${item.name.firstname} ${item.name.lastname}`}</Text>
+            <Text style={{ width: '30%' }}>{item.course}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {accountTable()}
+      {userTable()}
+      {studentTable()}
     </View>
   );
 }
@@ -14,7 +88,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10
+  },
+  tableLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10 
+  },
+  tableHeader: {
+    flexDirection: 'row',
+    backgroundColor: '#4cc9f0',
+    borderBottomWidth: 1,
+    padding: 10
+  },
+  tableData: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#caf0f8',
+  },
+  userContainer: {
+    height: 200,
+    marginTop: 30
+  },
+  studentContainer: {
+    marginTop: 30
+  },
+
+  //to alternate the color of the rows
+  tableDataEven: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: '#caf0f8', 
+    borderBottomWidth: 1,
+    borderBottomColor: '#caf0f8',
   },
 });
+
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    id: 1,
+    name: {
+      firstname: 'Arnold James',
+      lastname: 'Felisilda',
+    },
+    course: 'BSIT',
+    yearAndSection: '3A',
+    username: 'arnjames123',
+    password: 'arnold123',
+    usertype: 'Student'
+  },
+  {
+    id: 2,
+    name: {
+      firstname: 'Nicole James',
+      lastname: 'Paguican',
+    },
+    course: 'BSIT',
+    yearAndSection: '3A',
+    username: 'nicjames123',
+    password: 'nicole123',
+    usertype: 'Student'
+  },
+  {
+    id: 3,
+    name: {
+      firstname: 'Earl Mike',
+      lastname: 'Sarabia',
+    },
+    course: 'BSCS',
+    yearAndSection: '3A',
+    username: 'earlmike123',
+    password: 'mike123',
+    usertype: 'Student'
+  },
+  {
+    id: 4,
+    name: {
+      firstname: 'Rodel',
+      lastname: 'Garcia',
+    },
+    course: 'BSIT',
+    yearAndSection: '3A',
+    username: 'rodel123',
+    password: 'del123',
+    usertype: 'Student'
+  },
+  {
+    id: 5,
+    name: {
+      firstname: 'Marlon',
+      lastname: 'Carbonilla',
+    },
+    course: 'BSIT',
+    yearAndSection: '3A',
+    username: 'marlon123',
+    password: 'lon123',
+    usertype: 'Professor'
+  }
+]
+
+export default data;
